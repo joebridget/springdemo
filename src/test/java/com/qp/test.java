06f2f3148683552320298999b3fcadbdc32edfd4
joebridget <39877920+joebridget@users.noreply.github.com>
2019-04-18 09:06:07 +0800

    Update test.java

--- src/test/java/com/qp/test.java
+++ src/test/java/com/qp/test.java
@@ -3,6 +3,8 @@ package com.qp;
 public class test {
     public void test(){
         System.out.println(1);
-        System.out.println(1);
+        
+        System.out.println(2);
+        System.out.println(3);
     }
 }
